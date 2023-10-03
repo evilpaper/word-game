a37494a6adc2daef4b983f2976d8e774669856c6
Pelle Lundgren <pelle.lundgren@bubbi.ai>
2023-10-03 21:48:19 +0200

    Add guess input

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
+import GuessInput from "../GuessInput";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
+console.info(answer);
 
 function Game() {
-  return <>Put a game here!</>;
+  function handleSubmit(guess) {
+    console.log(`You guessed: ${guess}`);
+    if (guess === answer) {
+      alert("Correct!");
+    } else {
+      alert("Incorrect!");
+    }
+  }
+
+  return (
+    <>
+      <GuessInput handleSubmit={handleSubmit} />
+    </>
+  );
 }
 
 export default Game;
---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function GuessInput({ handleSubmit }) {
+  const [guess, setGuess] = React.useState("");
+
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(event) => {
+        event.preventDefault();
+        setGuess("");
+        handleSubmit(guess);
+      }}
+    >
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        pattern="[A-Za-z]{5}"
+        value={guess}
+        onChange={(event) => setGuess(event.target.value.toUpperCase())}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
